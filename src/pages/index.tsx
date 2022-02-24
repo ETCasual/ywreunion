a31837a07bd6a4a161d06e7d22dc3a0d230829e9
ETCasual <casualorient@gmail.com>
2022-02-25 06:07:06 +0800

    Template for tailwind + typescirpt

---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+const IndexPage = () => <h1 className="text-red-500">Hello Next.js 👋</h1>;
+
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,tsx,jsx}",
+    "./src/components/**/*.{js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
